af5eb2f050a44554894d7158dbd5864cba924d34
victor <victorreisprog@gmail.com>
2025-08-05 13:40:07 -0300

    feat: value semantics, pointer semantics, composition (embedding)

--- practical_go/module_3/game/game.go
+++ practical_go/module_3/game/game.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	var i Item
@@ -16,11 +19,36 @@ func main() {
 
 	fmt.Println(NewItem(10, 20))
 	fmt.Println(NewItem(10, 200))
+
+	i.Move(10, 20)
+
+	fmt.Println(i)
+
+	p1 := Player{
+		Name: "Victor",
+	}
+
+	fmt.Printf("p1: %+v\n", p1)
+	fmt.Println("p1.X:", p1.X)
+	p1.Move(100, 200)
+	fmt.Println("p1 (move) \n", p1)
+
+	fmt.Println(p1.Found("copper"))
+	fmt.Println(p1.Found("copper"))
+	fmt.Println(p1.Found("gold"))
+
+	fmt.Println("keys:", p1.Keys)
 }
 
 type Item struct {
 	X int
 	Y int
+}
+
+type Player struct {
+	Name string
+	Item // campo implicito (COMPOSITION)
+	Keys []string
 }
 
 /*
@@ -56,4 +84,37 @@ func NewItem(x, y int) (*Item, error) {
 	// passar as coisas por valor (!= de pointer semantics) em GO, geralmente vai te dar uma performance melhor
 	// pois vai aliviar o garbage collector
 	return &i, nil
+}
+
+// move i por delta x e delta y
+/*
+Value vs Pointer Receiver
+- Geralmente usa VALUE semantics
+- TENTA usar a mesma semantica em TODOS os metodos, se estiver usando value usar value,
+	se estiver usando pointer semantics continua usando pointer nos outros métodos
+
+- Quando DEVE usar pointer receiver:
+	- Se tiver um campo de LOCK
+	- Se precisar mutar a struct (alterar o valor nela)
+	- Decoding / unmarshaling
+*/
+func (i *Item) Move(dx, dy int) {
+	i.X += dx
+	i.Y += dy
+}
+
+// Pointer Semantics - vamos mutar o player
+func (p *Player) Found(key string) error {
+	switch key {
+	case "copper", "jade", "crystal":
+		// ok
+	default:
+		return fmt.Errorf("unknown key: %q", key)
+	}
+
+	if !slices.Contains(p.Keys, key) {
+		p.Keys = append(p.Keys, key)
+	}
+
+	return nil
 }
