c6040f123ff95d29b46489c03f04f161c3f3d253
victor <victorreisprog@gmail.com>
2025-12-29 19:58:33 -0300

    feat: quotas, run configs

--- service6-video/apis/services/sales/main.go
+++ service6-video/apis/services/sales/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/viquitorreis/service6-video/foundation/logger"
 )
 
@@ -46,6 +50,45 @@ func main() {
 func run(ctx context.Context, log *logger.Logger) error {
 	// GOMAXPROCS
 	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "build", build)
+
+	// CONFIG
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimetout      time.Duration `conf:"default:5s"`
+			WriteTimeout      time.Duration `conf:"default:5s"`
+			IdleTimeout       time.Duration `conf:"default:120s"`
+			ShutdownTimeout   time.Duration `conf:"default:20s"`
+			APIHost           string        `conf:"default:0.0.0.0:3000"`
+			DebugHost         string        `conf:"default:0.0.0.0:3010"`
+			CORSAllowdOrigins []string      `conf:"default:*,mask"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Sales",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Print(help)
+			return nil
+		}
+
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config:", out)
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
--- service6-video/go.mod
+++ service6-video/go.mod
@@ -2,4 +2,7 @@ module github.com/viquitorreis/service6-video
 
 go 1.25.5
 
-require github.com/go-json-experiment/json v0.0.0-20251027170946-4849db3c2f7e
+require (
+	github.com/ardanlabs/conf/v3 v3.10.0
+	github.com/go-json-experiment/json v0.0.0-20251027170946-4849db3c2f7e
+)
